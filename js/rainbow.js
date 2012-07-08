7639fd0b009dd19b6820874ad095ff6597421506
DouG Molidor <doug@creativelift.net>
2012-07-08 07:52:06 -0700

    Final rainbow functional, untested

--- js/rainbow.js
+++ js/rainbow.js
@@ -11,16 +11,16 @@ $(document).ready(function(){
 	//tl.staggerTo( $(".beams"), function(){ return durMin + Math.random()*(durMax-durMin) }, { css: { height: "100%" }, ease: Linear.easeNone }, 0 )
 	    
 	$("#intro .rainbow-layer .rainbow1.vert .beam").each(function(i,n){
-	    var dur = durMin + Math.random()*(durMax-durMin)
-	        t1.insert(TweenMax.to( n, (function(){ return durMin + Math.random()*(durMax-durMin) })(), { css: { height: "100%" }, delay: (function(){ return durMin + Math.random()*(durMax-durMin) })(), ease: Linear.easeNone }));
+	    var dur = durMin + Math.random()*(durMax-durMin);
+	    t1.insert(TweenMax.to( n, (function(){ return durMin + Math.random()*(durMax-durMin) })(), { css: { height: "100%" }, delay: (function(){ return durMin + Math.random()*(durMax-durMin) })(), ease: Linear.easeNone }));
 	});
 	$("#intro .rainbow-layer .horz .beam").each(function(i,n){
-	    var dur = durMin + Math.random()*(durMax-durMin)
-	        t2.insert(TweenMax.to( n, (function(){ return durMin + Math.random()*(durMax-durMin) })(), { css: { width: "100%" }, ease: Linear.easeNone }));
+	    var dur = durMin + Math.random()*(durMax-durMin);
+	    t2.insert(TweenMax.to( n, (function(){ return durMin + Math.random()*(durMax-durMin) })(), { css: { width: "100%" }, ease: Linear.easeNone }));
 	});
 	$("#intro .rainbow-layer .rainbow3.vert .beam").each(function(i,n){
-	    var dur = durMin + Math.random()*(durMax-durMin)
-	        t3.insert(TweenMax.to( n, (function(){ return durMin + Math.random()*(durMax-durMin) })(), { css: { height: "100%" }, delay: (function(){ return durMin + Math.random()*(durMax-durMin) })(), ease: Linear.easeNone }));
+	    var dur = durMin + Math.random()*(durMax-durMin);
+	    t3.insert(TweenMax.to( n, (function(){ return durMin + Math.random()*(durMax-durMin) })(), { css: { height: "100%" }, delay: (function(){ return durMin + Math.random()*(durMax-durMin) })(), ease: Linear.easeNone }));
 	});
 
 	// $("#intro .rainbow-layer .vert .beam").each(function(i,n){
@@ -43,9 +43,27 @@ $(document).ready(function(){
 		var tll = new TimelineMax();
 		$(".rainbow-layer .rainbow3.vert .beam", page).each(function(i,n){
 		    var dur = durMin + Math.random()*(durMax-durMin)
-		        tll.insert(TweenMax.to( n, (function(){ return durMin + Math.random()*(durMax-durMin) })(), { css: { height: "100%" }, delay: (function(){ return durMin + Math.random()*(durMax-durMin) })(), ease: Linear.easeNone }));
+		    tll.insert(TweenMax.to( n, (function(){ return durMin + Math.random()*(durMax-durMin) })(), { css: { height: "100%" }, delay: (function(){ return durMin + Math.random()*(durMax-durMin) })(), ease: Linear.easeNone }));
 		});
 		tl.append(tll);
+		tl.stop();
+		page.bind("play_rb", function(e){ tl.play() });
+		page.bind("rev_rb", function(e){ tl.reverse() });
+		return tl;
+	}
+
+	var makeFinalRainbow = function( page ) {
+		var ctaH = $(".cta").offset().top + $(".cta").height();
+		$("#fifth .rainbow-layer .rainbow3.vert").css({ "height": ctaH });
+		console.log("faaaak", ctaH);
+		var tl = makePageVertRainbow( page );
+		var t1 = new TimelineMax();
+		var t2 = new TimelineMax();
+		$(".rainbow-layer .rainbow2.horz .beam", page).each(function(i,n){
+			var dur = durMin + Math.random()*(durMax-durMin);
+	    	t2.insert(TweenMax.to( n, (function(){ return durMin + Math.random()*(durMax-durMin) })(), { css: { width: "100%" }, ease: Linear.easeNone }));
+		});
+		tl.append(t2);
 		tl.stop();
 		page.bind("play_rb", function(e){ tl.play() });
 		page.bind("rev_rb", function(e){ tl.reverse() });
@@ -55,6 +73,7 @@ $(document).ready(function(){
 	makePageVertRainbow( $("#second") );
 	makePageVertRainbow( $("#third") );
 	makePageVertRainbow( $("#fourth") );
-	makePageVertRainbow( $("#fifth") );
-
+	makeFinalRainbow( $("#fifth") );
+$(".cta").offset().top
+$("#fifth").offset().top
 });
